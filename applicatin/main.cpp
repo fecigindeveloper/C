3a7bdb51164f8f44a716b7577d8a62ab7fca491b
fecigindev <33804459+fecigindev@users.noreply.github.com>
2019-04-14 00:39:11 +0700

    Update main.cpp

--- applicatin/main.cpp
+++ applicatin/main.cpp
@@ -4,17 +4,35 @@
 using namespace std;
 
 int main(int args, char argv[] ) {
+  //OPEN database
   sqlite3 *db;
-  char *zErrMsg = 0;
+  char *msgError = 0;
   int rc;
   
   rc = sqlite3_open("test.db", &db);
   
   if( rc ) {  //jika database tidak bisa dibuka maka akan keluar error sqlite3 
-    fprintf(stderr, "Can't Open Database ", sqlite3_errmsg(db));
+    fprintf(stderr, "database tidak dapat dibuka  ", sqlite3_errmsg(db));
     return(0);
   } else { //jika database berhasil dibuka maka akan ada pesan open successfully
     fprintf(stderr, "Opened database successfully\n");
   }
   sqlite3_close(db);
+  //code open database selesai
+  
+  //code create table
+  sql = "CREATE TABLE COMPANY("
+    ID INT PRIMARY KEY NOT NULL,
+    NAME TEXT NOT NULL,
+    ADDRESS CHAR(50),
+    SALARY REAL
+  );";
+  
+  /*Execute SQL statement */
+  rc = sqlite3_exec(db, sql, callback, 0, &msgError);
+  
+  if(rc != SQLITE_OK) {
+    fprintf(stderr, "SQL error %s ",msgError);
+  }
+    
 }
