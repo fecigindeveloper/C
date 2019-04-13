2c9e9c03a24bb0660c658eacb1bf8faa9f3fe24b
fecigindev <33804459+fecigindev@users.noreply.github.com>
2019-04-14 00:07:49 +0700

    Update main.cpp

--- applicatin/main.cpp
+++ applicatin/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <stdio.h>
+#include <conio.h>
 using namespace std;
 
-main() {
-  cout<<"hello"<<endl;
-  getch();
+int main(int args, char argv[] ) {
+  sqlite3 *db;
+  char *zErrMsg = 0;
+  int rc;
+  
+  rc = sqlite3_open("test.db", &db);
+  
+  if( rc ) {  //jika database tidak bisa dibuka maka akan keluar error sqlite3 
+    fprintf(stderr, "Can't Open Database ", sqlite3_errmsg(db));
+    return(0);
+  } else { //jika database berhasil dibuka maka akan ada pesan open successfully
+    fprintf(stderr, "Opened database successfully\n");
+  }
+  sqlite3_close(db);
 }
